c30fdfb419f5e77f48e7e6c37b799419d147c69c
PC <doupig1@gmail.com>
2022-09-20 10:00:13 +0300

    added vosk

--- Friend/main.py
+++ Friend/main.py
@@ -1,1 +1,28 @@
-print('NO No')
+import json, pyaudio
+from vosk import Model, KaldiRecognizer
+import os
+import teleBot
+import tokens
+
+model = Model(r'model')
+rec = KaldiRecognizer(model, 16000)
+p = pyaudio.PyAudio()
+stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8000)
+
+stream.start_stream()
+
+
+def listen():
+    while True:
+        data = stream.read(4000, exception_on_overflow=False)
+        if (rec.AcceptWaveform(data)) and (len(data))>0:
+            answer = json.loads(rec.Result())
+            if answer['text']:
+                yield answer['text']
+
+print('GOOD')
+
+for text in listen():
+    print(text)
+
+
